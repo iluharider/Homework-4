d6fc1844164d2f31f29800a38c4e3e7e68afc871
Ilia Bondarenko <iluharider2002@gmail.com>
2023-12-24 00:47:14 +0300

    Add project files.

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿ var x = new
+ {
+    Items = new List<int> { 1, 2, 3 }.GetEnumerator()
+ };
+//while (x.Items.MoveNext()) // как было
+//   Console.WriteLine(x.Items.Current);
+var x_iterator = x.Items; // исправленный вариант - все время используем один и тот же enumerator
+while (x_iterator.MoveNext())
+    Console.WriteLine(x_iterator.Current);
+// изначальный код не работает, т.к., обращаясь к методу Items, мы каждый раз заново вызываем GetEnumerator(), а он, в свою очередь, возвращает каждый раз новый enumerator, 
+// который каждый раз заново ставится на место перед первым элементом коллекции (согласно документации на https://learn.microsoft.com/en-us/dotnet/api/system.collections.ienumerable.getenumerator?view=net-8.0)
+// В случае, когда мы еще не вызвали MoveNext(), значение Current не определено, и поэтому оно возвращает значение типа int по умолчанию, т.е. 0.
+// именно такой случай мы и видим в строке Console.WriteLine(x.Items.Current); т.к. вызываем в этой строчке заново GetEnumerator().
+// ну и наконец, x.Items.MoveNext() всегда возвращает True, ведь мы каждый раз заново вызываем GetEnumerator(), поэтому MoveNext() каждый раз перемещает enumerator с места перед списком на первый элемент списка (т.е. следующий элемент всегда есть, это число 1, поэтому MoveNext() возвращает каждый раз True) получается условие while всегда выполнено, отсюда бесконечный цикл.
+// именно поэтому код бесконечно пишет 0 в консоль.
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public abstract class AbstractClass // abstract class with an abstract method
+{
+    public abstract void Method(int value);
+}
+
+public interface Interface1
+{
+    void Method(int value);
+}
+
+public interface Interface2
+{
+    void Method(int value);
+}
+
+public class MyClass : AbstractClass, Interface1, Interface2 // class that inherits from two interfaces and abstract class
+{
+     public override void Method(int value) // implementation of method from AbstractClass
+    {
+        Console.WriteLine("Implementation from AbstractClass: " + value);
+    }
+
+    void Interface1.Method(int value)  // implementation of method from Interface1
+    {
+        Console.WriteLine("Implementation from Interface1: " + value);
+    }
+
+    void Interface2.Method(int value) // implementation of method from Interface2
+    {
+        Console.WriteLine("Implementation from Interface2: " + value);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass();
+
+        ((Interface1)myClass).Method(10); 
+        ((Interface2)myClass).Method(20); 
+        myClass.Method(30); 
+    }
+}
